b9bfc17d15c8012c1188a4077d35254881d13e92
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-03 08:45:18 +0700

    method get satu buku

--- index.js
+++ index.js
@@ -10,11 +10,21 @@ app.use(express.json());
 sequelize
   .sync()
   .then(() => console.log("Database & tabel siap"))
-  .catch((err) => console.error("ync gagal:", err));
+  .catch((err) => console.error("Sync gagal:", err));
 
 app.get("/buku", async (req, res) => {
   try {
     const buku = await Book.findAll();
+    res.json(buku);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/buku/:id", async (req, res) => {
+  try {
+    const buku = await Book.findByPk(req.params.id);
+    if (!buku) return res.status(404).json({ message: "Buku tidak ditemukan" });
     res.json(buku);
   } catch (error) {
     res.status(500).json({ error: error.message });
