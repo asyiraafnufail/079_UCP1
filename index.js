c2b9a58c16f137f616e23920daea30ee8364ba47
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-03 08:43:07 +0700

    sync database

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import sequelize from "./config/database.js";
 import Book from "./models/book.js";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+sequelize
+  .sync()
+  .then(() => console.log("🧱 Database & tabel siap"))
+  .catch((err) => console.error("❌ Sync gagal:", err));
