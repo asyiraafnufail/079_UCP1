95d69feea98c775ef97f63783eb3146d42cab596
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-03 08:38:11 +0700

    edit book.js

--- models/book.js
+++ models/book.js
@@ -1,35 +1,31 @@
-import DataTypes from "sequelize";
-import sequelize from "../config/database.js";
+import DataTypes from 'sequelize';
+import sequelize from '../config/database.js';
 
-const Book = sequelize.define(
-  "Book",
-  {
-    idbuku: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    judul: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    pengarang: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    tahun: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    bidang: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+const Book = sequelize.define('Book', {
+  idbuku: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
   },
-  {
-    tableName: "buku",
-    timestamps: false,
-  }
-);
+  judul: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  pengarang: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  tahun: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  bidang: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  tableName: 'buku',
+  timestamps: false,
+});
 
 export default Book;
